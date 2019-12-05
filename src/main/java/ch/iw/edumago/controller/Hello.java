cfeaa090d798a1ee58ea5fa9bdddf9cce2e3c793
itclub-oberland <itclub.oberland@gmail.com>
2019-12-05 23:54:46 +0100

    adding versioned path

--- src/main/java/ch/iw/edumago/controller/Hello.java
+++ src/main/java/ch/iw/edumago/controller/Hello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Hello {
 
-    @RequestMapping("/")
+    @RequestMapping("/api/v1/")
     public String home() {
         return "Hello Docker World";
     }
