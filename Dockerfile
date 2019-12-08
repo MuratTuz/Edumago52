6530a5b0b154f8c100ff92082bc3b4900932db89
itclub-oberland <itclub.oberland@gmail.com>
2019-12-08 21:21:39 +0100

    adding profiles

--- Dockerfile
+++ Dockerfile
@@ -6,11 +6,11 @@ RUN apk add maven
 
 COPY . .
 
-RUN mvn clean install
+RUN mvn package
 
 #ARG JAR_FILE=/root/.m2/repository/ch/iw/edumago/0.0.1-SNAPSHOT/*.jar
 #COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/root/.m2/repository/ch/iw/edumago/0.0.1-SNAPSHOT/edumago-0.0.1-SNAPSHOT.jar"]
+ENTRYPOINT ["java", "-jar", "-Djava.security.egd=file:/dev/./urandom", "-Dspring.profiles.active=prod", "/root/.m2/repository/ch/iw/edumago/0.0.1-SNAPSHOT/edumago-0.0.1-SNAPSHOT.jar"]
 
 
 # in order to build and run docker locally
--- src/main/java/ch/iw/edumago/EdumagoApplication.java
+++ src/main/java/ch/iw/edumago/EdumagoApplication.java
@@ -1,13 +1,23 @@
 package ch.iw.edumago;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
 
 @SpringBootApplication
-public class EdumagoApplication {
+public class EdumagoApplication implements CommandLineRunner {
+
+	@Autowired
+	private ServerProperties serverProperties;
+
+	@Override
+	public void run(String... args) {
+		System.out.println("EDUMAGO" + serverProperties.toString());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EdumagoApplication.class, args);
 	}
-
 }
