2212ff3777c3dfa1df34b556c2c2bc43b9c40b50
itclub-oberland <itclub.oberland@gmail.com>
2019-12-29 17:37:48 +0100

    merge conflicts

--- src/main/java/ch/iw/edumago/controller/VersionController.java
+++ src/main/java/ch/iw/edumago/controller/VersionController.java
@@ -13,31 +13,10 @@ public class VersionController {
     @GetMapping("/version")
     public ResponseEntity<VersionInfo> getVersion() {
 
-        /*
-        VersionInfo info = null;
-
-        try{
-            ApplicationContext context
-                    = new ClassPathXmlApplicationContext();
-
-                Resource file = context.getResource("classpath:/version.json");
-
-                if (file.exists())
-                {
-                    Gson gson = new Gson();
-
-                    info = gson.fromJson(new FileReader(file.getFile().getPath()), VersionInfo.class);
-                    return new ResponseEntity<VersionInfo>(info, HttpStatus.OK);
-                }
-            info = new VersionInfo();
-            info.setBuildId("There was an error: path not found");
-            return new ResponseEntity<VersionInfo>(info, HttpStatus.OK);
-        } catch (IOException e) {
-            info = new VersionInfo();
-            info.setBuildId("There was an error: " + e.getMessage());
-            return new ResponseEntity<VersionInfo>(info, HttpStatus.OK);
+        try {
+            return new ResponseEntity<VersionInfo>(new VersionInfo(), HttpStatus.OK);
+        }catch (Exception e) {
+            return new ResponseEntity<VersionInfo>(HttpStatus.NOT_FOUND);
         }
-         */
-        return new ResponseEntity<>(new VersionInfo(), HttpStatus.OK);
     }
 }
--- src/main/java/ch/iw/edumago/dto/VersionInfo.java
+++ src/main/java/ch/iw/edumago/dto/VersionInfo.java
@@ -1,40 +1,71 @@
 package ch.iw.edumago.dto;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class VersionInfo {
-    public String buildId;
-    public String startTime;
-    public String initiator;
-    public String buildNumber;
+    @Value("${AWS_DEFAULT_REGION:not_found}")
+    public String AWS_DEFAULT_REGION;
 
-    public String getBuildId() {
-        return buildId;
-    }
+    @Value("${AWS_REGION:not_found}")
+    public String AWS_REGION;
 
-    public void setBuildId(String buildId) {
-        this.buildId = buildId;
-    }
+    @Value("${CODEBUILD_BUILD_ARN:not_found}")
+    public String CODEBUILD_BUILD_ARN;
 
-    public String getStartTime() {
-        return startTime;
-    }
+    @Value("${CODEBUILD_BUILD_ID:not_found}")
+    public String CODEBUILD_BUILD_ID;
 
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
+    @Value("${CODEBUILD_BUILD_IMAGE:not_found}")
+    public String CODEBUILD_BUILD_IMAGE;
 
-    public String getInitiator() {
-        return initiator;
-    }
+    @Value("${CODEBUILD_BUILD_NUMBER:not_found}")
+    public String CODEBUILD_BUILD_NUMBER;
 
-    public void setInitiator(String initiator) {
-        this.initiator = initiator;
-    }
+    @Value("${CODEBUILD_BUILD_SUCCEEDING:not_found}")
+    public String CODEBUILD_BUILD_SUCCEEDING;
 
-    public String getBuildNumber() {
-        return buildNumber;
-    }
+    @Value("${CODEBUILD_INITIATOR:not_found}")
+    public String CODEBUILD_INITIATOR;
 
-    public void setBuildNumber(String buildNumber) {
-        this.buildNumber = buildNumber;
-    }
+    @Value("${CODEBUILD_KMS_KEY_ID:not_found}")
+    public String CODEBUILD_KMS_KEY_ID;
+
+    @Value("${CODEBUILD_LOG_PATH:not_found}")
+    public String CODEBUILD_LOG_PATH;
+
+    @Value("${CODEBUILD_RESOLVED_SOURCE_VERSION:not_found}")
+    public String CODEBUILD_RESOLVED_SOURCE_VERSION;
+
+    @Value("${CODEBUILD_SOURCE_REPO_URL:not_found}")
+    public String CODEBUILD_SOURCE_REPO_URL;
+
+    @Value("${CODEBUILD_SOURCE_VERSION:not_found}")
+    public String CODEBUILD_SOURCE_VERSION;
+
+    @Value("${CODEBUILD_SRC_DIR:not_found}")
+    public String CODEBUILD_SRC_DIR;
+
+    @Value("${CODEBUILD_START_TIME:not_found}")
+    public String CODEBUILD_START_TIME;
+
+    @Value("${CODEBUILD_WEBHOOK_ACTOR_ACCOUNT_ID:not_found}")
+    public String CODEBUILD_WEBHOOK_ACTOR_ACCOUNT_ID;
+
+    @Value("${CODEBUILD_WEBHOOK_BASE_REF:not_found}")
+    public String CODEBUILD_WEBHOOK_BASE_REF;
+
+    @Value("${CODEBUILD_WEBHOOK_EVENT:not_found}")
+    public String CODEBUILD_WEBHOOK_EVENT;
+
+    @Value("${CODEBUILD_WEBHOOK_PREV_COMMIT:not_found}")
+    public String CODEBUILD_WEBHOOK_PREV_COMMIT;
+
+    @Value("${CODEBUILD_WEBHOOK_HEAD_REF:not_found}")
+    public String CODEBUILD_WEBHOOK_HEAD_REF;
+
+    @Value("${CODEBUILD_WEBHOOK_TRIGGER:not_found}")
+    public String CODEBUILD_WEBHOOK_TRIGGER;
+
+    @Value("${HOME:not_found}")
+    public String HOME;
 }
